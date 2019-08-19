5e2ed1c3fa4ac7a923b77bef13c9cd58fef078c0
origin <aa3169638@qq.com>
2019-08-19 11:55:18 +0800

    2

---
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -0,0 +1,10 @@
+package com.pinyougou.sellergoods.service;
+
+import com.pinyougou.pojo.TbBrand;
+
+import java.util.List;
+
+public interface BrandService {
+
+    public List<TbBrand> findAll();
+}
