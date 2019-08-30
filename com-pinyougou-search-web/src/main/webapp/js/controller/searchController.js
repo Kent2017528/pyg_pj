8be5b08ada4875f61ab5a1565fc7355c50b1a7c8
origin <aa3169638@qq.com>
2019-08-30 10:22:21 +0800

    10.5

--- com-pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ com-pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -1,14 +1,17 @@
 app.controller('searchController',function($scope,searchService){
     //搜索
     $scope.search=function(){
+        $scope.searchMap.pageNo= parseInt($scope.searchMap.pageNo) ;
         searchService.search($scope.searchMap).success(
             function(response){
                 $scope.resultMap=response;//搜索返回的结果
+                buildPageLabel();//调用
             }
         );
     }
 
-    $scope.searchMap={'keywords':'','category':'','brand':'','spec':{},'price':''};//搜索对象
+    $scope.searchMap={'keywords':'','category':'','brand':'','spec':{},'price':'',
+        'pageNo':1,'pageSize':40};//搜索对象
 //添加搜索项
     $scope.addSearchItem=function(key,value){
         if(key=='category' || key=='brand' || key=='price'){//如果点击的是分类或者是品牌
@@ -27,5 +30,44 @@ app.controller('searchController',function($scope,searchService){
             delete $scope.searchMap.spec[key];//移除此属性
         }
         $scope.search();//执行搜索
+    }
+
+
+    //构建分页标签(totalPages为总页数)
+    buildPageLabel=function(){
+        $scope.pageLabel=[];//新增分页栏属性
+        var maxPageNo= $scope.resultMap.totalPages;//得到最后页码
+        var firstPage=1;//开始页码
+        var lastPage=maxPageNo;//截止页码
+        $scope.firstDot=true;//前面有点
+        $scope.lastDot=true;//后边有点
+        if($scope.resultMap.totalPages> 5){  //如果总页数大于5页,显示部分页码
+            if($scope.searchMap.pageNo<=3){//如果当前页小于等于3
+                lastPage=5; //前5页
+                $scope.firstDot=false;//前面没点
+            }else if( $scope.searchMap.pageNo>=lastPage-2  ){//如果当前页大于等于最大页码-2
+                firstPage= maxPageNo-4;		 //后5页
+                $scope.lastDot=false;//后边没点
+            }else{ //显示当前页为中心的5页
+                firstPage=$scope.searchMap.pageNo-2;
+                lastPage=$scope.searchMap.pageNo+2;
+            }
+        }else {
+            $scope.firstDot=false;//前面无点
+            $scope.lastDot=false;//后边无点
+        }
+        //循环产生页码标签
+        for(var i=firstPage;i<=lastPage;i++){
+            $scope.pageLabel.push(i);
+        }
+    };
+    //根据页码查询
+    $scope.queryByPage=function(pageNo){
+        //页码验证
+        if(pageNo<1 || pageNo>$scope.resultMap.totalPages){
+            return;
+        }
+        $scope.searchMap.pageNo=pageNo;
+        $scope.search();
     }
 });
